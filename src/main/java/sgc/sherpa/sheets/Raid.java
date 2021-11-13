ec3cae112074eb385e810b39f825eefeaadabdf5
Chris Hoppe <hoppefalcon@gmail.com>
2021-11-13 14:52:10 -0500

    Updating Raid Hashes

--- src/main/java/sgc/sherpa/sheets/Raid.java
+++ src/main/java/sgc/sherpa/sheets/Raid.java
@@ -13,10 +13,10 @@ import java.util.List;
  * @author chris hoppe
  */
 public enum Raid {
-    LAST_WISH("Last Wish", Arrays.asList("2122313384", "1661734046")),
-    GARDEN_OF_SALVATION("Garden of Salvation", Arrays.asList("2659723068", "3458480158", "2497200493")),
+    LAST_WISH("Last Wish", Arrays.asList("2214608157", "2214608156", "2122313384", "1661734046")),
+    GARDEN_OF_SALVATION("Garden of Salvation", Arrays.asList("3845997235", "2659723068", "3458480158", "2497200493")),
     DEEP_STONE_CRYPT("Deep Stone Crypt", Arrays.asList("910380154", "3976949817")),
-    VAULT_OF_GLASS("Vault of Glass", Arrays.asList("3881495763", "1485585878", "1681562271"));
+    VAULT_OF_GLASS("Vault of Glass", Arrays.asList("3881495763", "1485585878", "1681562271", "3711931140"));
 
     public final String name;
     private final List<String> validHashes;
