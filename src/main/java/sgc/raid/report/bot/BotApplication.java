bf1e31311dc7886b80e082fea6663e329d1210ab
Chris Hoppe <hoppefalcon@gmail.com>
2022-04-08 16:24:37 -0400

    Update BotApplication.java

--- src/main/java/sgc/raid/report/bot/BotApplication.java
+++ src/main/java/sgc/raid/report/bot/BotApplication.java
@@ -97,10 +97,11 @@ public class BotApplication {
 						"Pulls the Weekly Raid Report of the user. (Requires full Bungie ID, Start Date, and End Date)")
 						.addOption(bungieIdOption.build()).addOption(userWeeklyClearStartOption.build())
 						.addOption(userWeeklyClearEndOption.build()),
-				new SlashCommandBuilder().setName("sgc-activity-report").setDescription(
-						"Pulls the Weekly Activity Report for the SGC. (Requires Start Date, and End Date)")
-						.addOption(userWeeklyClearStartOption.build())
-						.addOption(userWeeklyClearEndOption.build()),
+				// new SlashCommandBuilder().setName("sgc-activity-report").setDescription(
+				// "Pulls the Weekly Activity Report for the SGC. (Requires Start Date, and End
+				// Date)")
+				// .addOption(userWeeklyClearStartOption.build())
+				// .addOption(userWeeklyClearEndOption.build()),
 				new SlashCommandBuilder().setName("raid-carnage-report")
 						.setDescription("Pulls a Full Raid Carnage Report.").addOption(carnageIdOption.build())))
 				.join();
