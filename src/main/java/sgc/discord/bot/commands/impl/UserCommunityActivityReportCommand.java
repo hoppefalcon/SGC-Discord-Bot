e94b8ab9444c517101054eae5efa37b9c402bcdb
Chris Hoppe <hoppefalcon@gmail.com>
2022-05-15 17:00:01 -0400

    Update UserCommunityActivityReportCommand.java

--- src/main/java/sgc/discord/bot/commands/impl/UserCommunityActivityReportCommand.java
+++ src/main/java/sgc/discord/bot/commands/impl/UserCommunityActivityReportCommand.java
@@ -21,10 +21,6 @@ public class UserCommunityActivityReportCommand implements Command {
 
         @Override
         public void handle(SlashCommandInteraction slashCommandInteraction) {
-                LOGGER.info(slashCommandInteraction.getOptionByName("BungieID").get().getStringRepresentationValue()
-                                .get());
-                LOGGER.info(slashCommandInteraction.getOptionByName("BungieID").get().getStringValue()
-                                .get());
                 String userBungieId = slashCommandInteraction.getOptionByName("BungieID").get().getStringValue().get();
                 String startDateStr = slashCommandInteraction.getOptionByName("StartDate").get().getStringValue().get();
                 String endDateStr = slashCommandInteraction.getOptionByName("EndDate").get().getStringValue().get();
