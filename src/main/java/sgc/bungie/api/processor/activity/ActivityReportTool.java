e98b4510d3ff286f9999634d85101fb23ce2bd3c
Chris Hoppe <hoppefalcon@gmail.com>
2022-12-06 21:37:27 -0500

    Switching to Google Service Account

--- src/main/java/sgc/bungie/api/processor/activity/ActivityReportTool.java
+++ src/main/java/sgc/bungie/api/processor/activity/ActivityReportTool.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
@@ -26,21 +25,19 @@ import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.user.User;
 import org.slf4j.Logger;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.Sheets.Spreadsheets.Values.Update;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.BatchClearValuesRequest;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 
 import sgc.SGC_Clan;
 import sgc.bungie.api.processor.RaidReportTool;
@@ -51,7 +48,6 @@ public class ActivityReportTool {
     private static final Logger LOGGER = BotApplication.getLogger();
     private static final String APPLICATION_NAME = "SGC Bot";
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
 
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
@@ -89,8 +85,9 @@ public class ActivityReportTool {
         try {
             LOGGER.info("Initiating Google Sheet Auth");
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
             Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                    getGoogleSheetsCredentials(HTTP_TRANSPORT))
+                    getGoogleSheetsHttpRequestInitializer())
                     .setApplicationName(APPLICATION_NAME)
                     .build();
 
@@ -180,23 +177,18 @@ public class ActivityReportTool {
      * @return
      * @throws IOException
      */
-    private static Credential getGoogleSheetsCredentials(final NetHttpTransport HTTP_TRANSPORT)
+    private static HttpRequestInitializer getGoogleSheetsHttpRequestInitializer()
             throws IOException {
-        // Load client secrets.
-        InputStream in = ActivityReportTool.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
+        // Load service account secrets.
+        InputStream resourceAsStream = ActivityReportTool.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (resourceAsStream == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        final ServiceAccountCredentials serviceAccountCredentials = ServiceAccountCredentials
+                .fromStream(resourceAsStream);
+        GoogleCredentials credentials = serviceAccountCredentials.createScoped(SCOPES);
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        return new HttpCredentialsAdapter(credentials);
     }
 
     /**
@@ -209,7 +201,7 @@ public class ActivityReportTool {
             final String spreadsheetId = "1R0RkQYKVWcy6DA71xNkoDU-XIc14UjXuD7M05H2VHPk";
 
             Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                    getGoogleSheetsCredentials(HTTP_TRANSPORT))
+                    getGoogleSheetsHttpRequestInitializer())
                     .setApplicationName(APPLICATION_NAME)
                     .build();
 
