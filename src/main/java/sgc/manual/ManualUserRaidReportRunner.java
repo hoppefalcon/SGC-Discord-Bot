3699c2a6aabd9193d7f473c011e406aa79abb4a8
Chris Hoppe <hoppefalcon@gmail.com>
2026-07-21 18:01:58 -0400

    Adding more pantheon info

--- src/main/java/sgc/manual/ManualUserRaidReportRunner.java
+++ src/main/java/sgc/manual/ManualUserRaidReportRunner.java
@@ -10,7 +10,7 @@ public class ManualUserRaidReportRunner {
 
         public static void main(String[] args) throws Exception {
                 try {
-                        String userBungieId = "Mister Wrecked#2123";
+                        String userBungieId = "hoppefalcon#7599";
                         String userWeeklyClears = RaidReportTool.getUserRaidReport(userBungieId);
                         System.out.println(userWeeklyClears);
                 } catch (Exception e) {
